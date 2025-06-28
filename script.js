0dc426877951fc51b6b029ed8d0f62090ac039e1
ragul5858 <ragulswamy03@gmail.com>
2025-06-28 15:26:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Highlight current section on scroll
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-links a");
+
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
